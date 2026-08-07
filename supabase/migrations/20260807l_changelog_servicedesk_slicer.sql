8ce1c29b8e885883daf2ae57384416c834289672
Tom Kamminga <tom@everts.chat>
2026-08-07 14:04:17 +0200

    Changelog: servicedesk-slicer op Aanvragen en Offertes

---
+++ supabase/migrations/20260807l_changelog_servicedesk_slicer.sql
@@ -0,0 +1,3 @@
+insert into public.changelog (datum, categorie, module, titel, omschrijving) values
+  ('2026-08-07','verbeterd','Dossiers','Servicedesk apart filteren op Aanvragen en Offertes',
+   'Bovenin de Aanvragen- en Offertes-tab staat een nieuwe filterbalk "Soort" met de knoppen Projecten en Servicedesk, elk met het aantal erachter. Eén klik en je ziet alleen die soort dossiers; nog een klik en alles is weer zichtbaar. Werkt zowel op het bord als in de lijstweergave en is te combineren met de filters op persoon en controller.');
